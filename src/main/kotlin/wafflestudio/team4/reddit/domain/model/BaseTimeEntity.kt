ddff0e944affaf33e53a8272d17f5ca122105c4a
PJH6029 <top231902@naver.com>
2021-11-26 14:01:02 +0900

    style: Reformat files
    - reformat files according to ktlint

--- src/main/kotlin/wafflestudio/team4/reddit/domain/model/BaseTimeEntity.kt
+++ src/main/kotlin/wafflestudio/team4/reddit/domain/model/BaseTimeEntity.kt
@@ -4,11 +4,15 @@ import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
-import javax.persistence.*
+import javax.persistence.EntityListeners
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
-open class BaseTimeEntity (
+open class BaseTimeEntity(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     open val id: Long = 0,
@@ -17,5 +21,5 @@ open class BaseTimeEntity (
     open var createdAt: LocalDateTime? = LocalDateTime.now(),
 
     @LastModifiedDate
-    open var updatedAt: LocalDateTime?= LocalDateTime.now()
+    open var updatedAt: LocalDateTime? = LocalDateTime.now()
 )
