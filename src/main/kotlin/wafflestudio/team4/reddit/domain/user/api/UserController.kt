40b9ab034d33b787fc440a5ec2a2f8ff63363ac5
PJH6029 <top231902@naver.com>
2021-12-11 12:06:01 +0900

    style: Refactor the code style

--- src/main/kotlin/wafflestudio/team4/reddit/domain/user/api/UserController.kt
+++ src/main/kotlin/wafflestudio/team4/reddit/domain/user/api/UserController.kt
@@ -3,7 +3,15 @@ package wafflestudio.team4.reddit.domain.user.api
 import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseEntity
 import org.springframework.transaction.annotation.Transactional
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
 import wafflestudio.team4.reddit.domain.user.dto.UserDto
 import wafflestudio.team4.reddit.domain.user.service.UserService
 import javax.validation.Valid
@@ -14,23 +22,16 @@ class UserController(
     private val userService: UserService,
 ) {
     @GetMapping("/")
-    fun getUsers(
-
-    ) {
-
+    fun getUsers() {
     }
 
     @GetMapping("/me/")
     @Transactional
-    fun getCurrentUser(
-
-    ) {
-
+    fun getCurrentUser() {
     }
 
     @GetMapping("/{user_id}/")
     fun getUser(@PathVariable("user_id") id: Long) {
-
     }
 
     @PostMapping("/users/signup/")
@@ -43,16 +44,13 @@ class UserController(
 
     @PostMapping("/users/signin/")
     fun signin() {
-
     }
 
     @DeleteMapping("/users/me/")
     fun deleteUser() {
-
     }
 
     @PutMapping("/users/me/")
     fun updateUser() {
-
     }
 }
