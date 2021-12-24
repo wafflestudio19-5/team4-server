427e2e4b90f1ddb137bac9575151144c3eb6ede6
PJH6029 <top231902@naver.com>
2021-12-24 10:04:53 +0900

    fix: Comment out DataLoader.kt
    * 필요 시 주석 해제 후 사용

--- src/main/kotlin/wafflestudio/team4/reddit/DataLoader.kt
+++ src/main/kotlin/wafflestudio/team4/reddit/DataLoader.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
-import wafflestudio.team4.reddit.domain.user.model.User
+// import wafflestudio.team4.reddit.domain.user.model.User
 import wafflestudio.team4.reddit.domain.user.repository.UserRepository
 
 @Component
@@ -13,10 +13,10 @@ class DataLoader(
     private val passwordEncoder: PasswordEncoder,
 ) : ApplicationRunner {
     override fun run(args: ApplicationArguments?) {
-        for (i: Int in 1..100) {
-            val username = "user$i"
-            val user = User("$username@snu.ac.kr", username, passwordEncoder.encode("somepassword"))
-            userRepository.save(user)
-        }
+//        for (i: Int in 1..100) {
+//            val username = "user$i"
+//            val user = User("$username@snu.ac.kr", username, passwordEncoder.encode("somepassword"))
+//            userRepository.save(user)
+//        }
     }
 }
