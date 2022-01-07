e2f049dc4385180783bbea581e8d53e5dd8ab466
PJH6029 <top231902@naver.com>
2022-01-08 00:07:10 +0900

    feat: Rewrite url

--- src/main/kotlin/wafflestudio/team4/reddit/global/oauth/controller/OAuthController.kt
+++ src/main/kotlin/wafflestudio/team4/reddit/global/oauth/controller/OAuthController.kt
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import wafflestudio.team4.reddit.domain.user.dto.UserDto
@@ -12,10 +14,12 @@ import wafflestudio.team4.reddit.global.auth.jwt.JwtTokenProvider
 import wafflestudio.team4.reddit.global.oauth.service.OAuthService
 
 @RestController
+@RequestMapping("/api/v1/social_login")
 class OAuthController(
     private val oAuthService: OAuthService,
     private val jwtTokenProvider: JwtTokenProvider,
 ) {
+    // TODO real oauth2
     @GetMapping("/signin/oauth/{provider}")
     fun signin(
         @PathVariable provider: String,
@@ -25,5 +29,9 @@ class OAuthController(
         val headers = HttpHeaders()
         headers.set("Authentication", jwtTokenProvider.generateToken(user.email)) // TODO refresh token
         return ResponseEntity(UserDto.Response(user), headers, HttpStatus.OK)
+    }
+
+    @PostMapping("/google/")
+    fun socialLogin() {
     }
 }
