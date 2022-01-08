3a3bc2b535833fe1213d1ef8cca6a9deba7f40b9
Yung Son <d3pshot17@gmail.com>
2022-01-08 10:34:19 +0900

    feat: Add attributes on responses of comments and posts

--- src/main/kotlin/wafflestudio/team4/reddit/domain/post/dto/PostDto.kt
+++ src/main/kotlin/wafflestudio/team4/reddit/domain/post/dto/PostDto.kt
@@ -1,6 +1,7 @@
 package wafflestudio.team4.reddit.domain.post.dto
 
 import wafflestudio.team4.reddit.domain.post.model.Post
+import java.time.LocalDateTime
 import javax.validation.constraints.NotBlank
 
 class PostDto {
@@ -8,22 +9,28 @@ class PostDto {
         val id: Long,
         val userId: Long,
         val community: String,
+        val userName: String,
+        val userImageUrl: String?,
         val title: String,
         val text: String?,
         val images: List<String>?, // S3 이미지 주소
         val numUpVotes: Int,
         val numDownVotes: Int,
+        val createdAt: LocalDateTime?
 //        val isDeleted: Boolean,
     ) {
         constructor(post: Post) : this(
             id = post.id,
             userId = post.user.id,
             community = post.community.name,
+            userName = post.user.username,
+            userImageUrl = post.user.userProfile?.userImage?.url,
             title = post.title,
             text = post.text,
             images = post.images?.map { it.url }, // S3적용 후 변경
             numUpVotes = post.votes.count { it.isUp == 2 },
             numDownVotes = post.votes.count { it.isUp == 0 },
+            createdAt = post.createdAt
 //            isDeleted = post.deleted
         )
     }
