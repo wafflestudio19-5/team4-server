87496de6d69bc54769ed6c0a8427e786ef622637
Yung Son <d3pshot17@gmail.com>
2022-01-29 04:28:34 +0900

    feat: Alter comparators to deal with remains after sorting

--- src/main/kotlin/wafflestudio/team4/reddit/domain/comment/service/CommentComparator.kt
+++ src/main/kotlin/wafflestudio/team4/reddit/domain/comment/service/CommentComparator.kt
@@ -10,8 +10,12 @@ class CommentComparator : Comparator<Comment> {
             val c0Popularity = c0.votes.map { if (it.isUp == 2) 2 else if (it.isUp == 0) -1 else 0 }.sum()
             val c1Popularity = c1.votes.map { if (it.isUp == 2) 2 else if (it.isUp == 0) -1 else 0 }.sum()
             if (c0Popularity < c1Popularity) return 1
-            else if (c0Popularity == c1Popularity) return 0
-            else return -1
+            else if (c0Popularity == c1Popularity) {
+                // 같으면 나머진 최신순
+                if (c0.id < c1.id) return 1
+                else if (c0.id == c1.id) return 0
+                else return -1
+            } else return -1
         }
 
         if (c0.id < c1.id) return 1
